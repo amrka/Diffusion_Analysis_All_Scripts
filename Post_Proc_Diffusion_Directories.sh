26973fd2fd529a22de934067db8c11ce445b2db2
amrka <amr.kamel@uv.es>
2019-02-11 19:36:07 +0100

    Update Post_Proc_Diffusion_Directories.sh

--- Post_Proc_Diffusion_Directories.sh
+++ Post_Proc_Diffusion_Directories.sh
@@ -14,7 +14,7 @@ mkdir -p CHARMED/{CHARMED_FR,CHARMED_FA,CHARMED_MD,CHARMED_RD,CHARMED_AD,CHARMED
 mkdir -p Diffusion_20/{Diffusion_20_FA,Diffusion_20_MD,Diffusion_20_RD,Diffusion_20_AD} 
 mkdir -p Kurtosis/{Kurtosis_FA,Kurtosis_MD,Kurtosis_RD,Kurtosis_AD,Kurtosis_MK,Kurtosis_AK,Kurtosis_RK,Kurtosis_AWF,Kurtosis_TORT} 
 mkdir -p NODDI/{NODDI_ODI,NODDI_FICVF}
-mkdir -p Kurtosis_Explore_DTI/{Kurtosis_FA,Kurtosis_MD,Kurtosis_RD,Kurtosis_AD,Kurtosis_MK,Kurtosis_AK,Kurtosis_RK,Kurtosis_KA,Kurtosis_AWF,Kurtosis_TORT}
+mkdir -p Kurtosis_Explore_DTI/{Kurtosis_E_DTI_FA,Kurtosis_E_DTI_MD,Kurtosis_E_DTI_RD,Kurtosis_E_DTI_AD,Kurtosis_E_DTI_MK,Kurtosis_E_DTI_AK,Kurtosis_E_DTI_RK,Kurtosis_E_DTI_KA,Kurtosis_E_DTI_AWF,Kurtosis_E_DTI_TORT}
 
 
 
